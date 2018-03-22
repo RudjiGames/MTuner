d8e7aa7cd7b0aa7f6003444727cb0f92e910e69d
Milos Tosic <milostosic77@gmail.com>
2018-03-22 08:08:37 +0800

    Update version to 4.1.0

--- src/version.h
+++ src/version.h
@@ -6,6 +6,6 @@
 #ifndef RTM_MTUNER_VERSION_H
 #define RTM_MTUNER_VERSION_H
 
-static const char* MTunerVersion = "4.0.9";
+static const char* MTunerVersion = "4.1.0";
 
 #endif // RTM_MTUNER_VERSION_H
