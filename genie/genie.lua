c7e5a959504195c5d3d02b06e5b37c738eb8eb19
Milos Tosic <milostosic77@gmail.com>
2018-07-12 10:37:34 +0800

    Simplify searching for 'build'

--- genie/genie.lua
+++ genie/genie.lua
@@ -3,12 +3,10 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
-local scriptDir = path.getdirectory(debug.getinfo(2, "S").source:sub(2)) .. "/.."
-local relativePath = scriptDir
-for v=1,6,1 do
-	relativePath = relativePath .. "/.."
-	local globals_script = path.getabsolute(relativePath) .. "/build/globals.lua"
-	if os.isfile(globals_script) then dofile (globals_script) break end
+local currPath = path.getdirectory(debug.getinfo(2, "S").source:sub(2)) .. "/.."
+while string.len(currPath) > 2 do 
+	currPath = path.getabsolute(currPath .. "/..")
+	if os.isfile(currPath .. "/build/build.lua") then dofile (currPath .. "/build/build.lua") break end
 end
 
 solution "MTuner"
