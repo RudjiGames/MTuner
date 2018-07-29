bb815aa4e6522d03637018da2ea521c3f1cb9d80
milostosic <milostosic77@gmail.com>
2018-07-30 00:43:16 +0800

    Update version and copyright info

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 //--------------------------------------------------------------------------//
-/// Copyright (c) 2017 by Milos Tosic. All Rights Reserved.                ///
+/// Copyright (c) 2018 by Milos Tosic. All Rights Reserved.                ///
 /// License: http://www.opensource.org/licenses/BSD-2-Clause               ///
 //--------------------------------------------------------------------------//
 
 #ifndef RTM_MTUNER_VERSION_H
 #define RTM_MTUNER_VERSION_H
 
-static const char* MTunerVersion = "4.1.1";
+static const char* MTunerVersion = "4.1.2";
 
 #endif // RTM_MTUNER_VERSION_H
