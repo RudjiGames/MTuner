e9f0606fe6a733c87098a912da93379f8c7e7a58
Milos Tosic <milostosic77@gmail.com>
2019-06-06 11:35:54 +0800

    Fix for high DPI scaling

--- src/mtunermain.cpp
+++ src/mtunermain.cpp
@@ -383,6 +383,16 @@ void shutDown();
 
 int main(int argc, const char* argv[])
 {
+#ifdef Q_OS_WIN
+	SetProcessDPIAware(); // call before the main event loop
+#endif // Q_OS_WIN 
+
+#if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
+	QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+#else
+	qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("1"));
+#endif // QT_VERSION
+
 	rtm::mtunerLoaderInit(true);
 	int ret = 0;
 
