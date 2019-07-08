f8ca8f0a5c547e5d94f57f5f6124a3fba78e994f
Milos Tosic <milostosic77@gmail.com>
2019-07-08 23:37:59 +0800

    Change path to Qt for localization batch

--- src/mtunerlang.bat
+++ src/mtunerlang.bat
@@ -1,3 +1,3 @@
-c:\Qt\Qt5.12.4\5.12.4\msvc2017_64\bin\lupdate.exe -verbose -no-obsolete mtunerlang.pro
-c:\Qt\Qt5.12.4\5.12.4\msvc2017_64\bin\linguist.exe mtuner_en.ts mtuner_rs.ts mtuner_it.ts mtuner_de.ts
-c:\Qt\Qt5.12.4\5.12.4\msvc2017_64\bin\lrelease mtunerlang.pro
+%QTDIR_VS2017_x64%\bin\lupdate.exe -verbose -no-obsolete mtunerlang.pro
+%QTDIR_VS2017_x64%\bin\linguist.exe mtuner_en.ts mtuner_rs.ts mtuner_it.ts mtuner_de.ts
+%QTDIR_VS2017_x64%\bin\lrelease mtunerlang.pro
