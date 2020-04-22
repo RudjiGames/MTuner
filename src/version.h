cca5fbaec387a8015e94b4f759aeb3ca671759f8
Milos Tosic <milostosic77@gmail.com>
2020-04-23 06:14:36 +0800

    Bump version

--- src/version.h
+++ src/version.h
@@ -6,6 +6,6 @@
 #ifndef RTM_MTUNER_VERSION_H
 #define RTM_MTUNER_VERSION_H
 
-static const char* MTunerVersion = "4.3.8";
+static const char* MTunerVersion = "4.4.0";
 
 #endif // RTM_MTUNER_VERSION_H
