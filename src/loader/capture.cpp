e82ca11c713610f46ea6cfd425f3a6284596c755
Milos Tosic <milostosic77@gmail.com>
2020-05-24 19:45:14 +0800

    Bug fix: zero sized module info was treated as corrupted capture. Version bump

--- src/loader/capture.cpp
+++ src/loader/capture.cpp
@@ -1122,8 +1122,11 @@ bool Capture::loadModuleInfo(BinLoader& _loader, uint64_t _fileSize)
 
 	uint8_t charSize;
 	_loader.readVar(charSize);
-	--symSize;
 
+	if (!symSize)
+		return true;
+
+	--symSize;
 	while (symSize > 0)
 	{
 		char16_t	exePath[1024];
--- src/version.h
+++ src/version.h
@@ -6,6 +6,6 @@
 #ifndef RTM_MTUNER_VERSION_H
 #define RTM_MTUNER_VERSION_H
 
-static const char* MTunerVersion = "4.4.3";
+static const char* MTunerVersion = "4.4.4";
 
 #endif // RTM_MTUNER_VERSION_H
