5437db128c7b7abe45220bffed86c76671c3fc92
Milos Tosic <milostosic77@gmail.com>
2022-09-11 11:40:21 +0200

    4.4.9 version

--- src/version.h
+++ src/version.h
@@ -6,6 +6,6 @@
 #ifndef RTM_MTUNER_VERSION_H
 #define RTM_MTUNER_VERSION_H
 
-static const char* MTunerVersion = "4.5.0";
+static const char* MTunerVersion = "4.4.9";
 
 #endif // RTM_MTUNER_VERSION_H
