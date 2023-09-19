4178e053e6cb95e49af2bb7acecc599a8fa56c5a
Milos Tosic <milostosic77@gmail.com>
2023-09-19 22:40:07 +0200

    Improved search for build

--- src/MTuner/genie/genie.lua
+++ src/MTuner/genie/genie.lua
@@ -3,11 +3,10 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
-local currPath = path.getdirectory(debug.getinfo(2, "S").source:sub(2)) .. "/.."
-while string.len(currPath) > 2 do 
-	currPath = path.getabsolute(currPath .. "/..")
-	if os.isfile(currPath .. "/build/build.lua") then dofile (currPath .. "/build/build.lua") break end
-end
+local cp = (function(x) return debug.getinfo(2, "S").source:sub(2):match("(.*[/\\])")  end)()
+while os.isdir(cp) do cp = path.getabsolute(cp .. "/..") local tp = cp .. "/build/build.lua"
+if os.isfile(tp) then dofile (tp) break end end if RTM_ROOT_DIR == nil then
+print("EROR: dependency missing - build. Cannot continue.") os.exit() end
 
 solution "MTuner"
 	configurations { "debug", "release", "retail" }
@@ -19,4 +18,3 @@ solution "MTuner"
 	loadProject( "MTunerInject" )
 	
 	startproject "MTuner"
-	
